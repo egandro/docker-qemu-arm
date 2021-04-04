efe39b388dc00f173ae6dfd9f87845384269505c
egandro <egandro@gmail.com>
2021-04-04 04:57:36 +0200

    Update pi-setup.sh

--- pi-setup.sh
+++ pi-setup.sh
@@ -5,7 +5,7 @@
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 7638D0442B90D010
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC
 
-rm -f /etc/apt/sources.list.d/buster-backports.list
+sudo rm -f /etc/apt/sources.list.d/buster-backports.list
 echo "deb http://deb.debian.org/debian buster-backports main" | sudo tee -a /etc/apt/sources.list.d/buster-backports.list
 
 sudo apt-get update
