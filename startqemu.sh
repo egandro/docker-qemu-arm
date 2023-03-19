8eaf26f052f577c0db540de8b3f0a62fd63ce7a1
Deiki <DeicPro@users.noreply.github.com>
2023-03-19 22:58:50 +0100

    Update startqemu.sh

--- startqemu.sh
+++ startqemu.sh
@@ -1,4 +1,4 @@
 qemu-system-x86_64 -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 \
   -drive if=pflash,format=raw,read-only,file=$PREFIX/share/qemu/edk2-x86_64-code.fd \
-  -netdev user,id=n1,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 \
+  -netdev user,id=n1,hostfwd=tcp::2222-:22,net=192.168.50.0/24 -device virtio-net,netdev=n1 \
   -nographic alpine.img
